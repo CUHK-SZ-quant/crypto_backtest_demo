213538db499e6032718270fe12540b672a426167
bradleyboyuyang <66737059+bradleyboyuyang@users.noreply.github.com>
2022-09-20 17:48:33 +0800

    update

--- 1_BTC市值图/plot_btc.py
+++ 1_BTC市值图/plot_btc.py
@@ -1,8 +1,10 @@
 # -*- encoding: utf-8 -*-
 '''
-@Description: 
-@Date: 2022/09/20 17:42:46
+@Description: Plot MV of BTC
 @Author: Yang Boyu
+@Email: bradleyboyuyang@gmail.com
+@File: plot_btc.py
+@Date: 2022/09/20 17:47:06
 @version: 1.0
 '''
 
@@ -10,15 +12,10 @@
 import pyecharts.options as opts
 import pandas as pd
 from pyecharts.charts import Line
-import os
 
-pd.set_option('expand_frame_repr', True) # pycharm设置为False可以产生滑条，但vscode不能产生滑条所以会堆叠。因此vscode应该设置为True
 pd.set_option('display.unicode.ambiguous_as_wide', True)
 pd.set_option('display.unicode.east_asian_width', True)
 pd.set_option('display.width', 180) # 每一行的宽度（避免换行）
-pd.set_option('display.max_rows', 100) # 显示的最大行数（避免只显示部分行数据）
-pd.set_option('display.max_columns', 100) # 显示的最大列数（避免列显示不全）
-# pd.set_option('display.float_format', lambda x: '%.3f' % x)    # 取消Pandas 科学计数法显示 + 可调节位数
 
 
 df = pd.read_csv("btc_mv.csv", encoding='gbk')
@@ -48,7 +45,6 @@ y_data = df['总市值（亿美元）']
     )
     .render("btc_mv_plot.html")
 )
-
 
 
     
